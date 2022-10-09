1e93562512999c1bf16e73de25da1be2668d04dd
Rasyid Miftahul Ihsan <rasyidmihsan@gmail.com>
2022-10-09 16:09:05 +0700

    Try to redeploy (2), but serviceaccount file still exposed

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
-    // console.log(`Phase: ${process.env.phase}`);
+    console.log(`Phase: ${process.env.phase}`);
   });
