143fe18979a1b64e182337a13061641a5c1afcc3
Rasyid Miftahul Ihsan <rasyidmihsan@gmail.com>
2022-10-10 23:40:42 +0700

    ADD: add new feature for creating class

--- src/middlewares/user-authorization.js
+++ src/middlewares/user-authorization.js
@@ -1,11 +1,22 @@
 const firebaseAdmin = require("firebase-admin");
 
-const userAuthorization = (req, res, next) => {
-  const token = firebaseAdmin.auth().verifyIdToken(req.header("user-token"));
-
-  if (token != null) {
-    next();
+const userAuthorization = async (req, res, next) => {
+  const userToken = req.header("user-token");
+  if (userToken != null && userToken != "") {
+    firebaseAdmin
+      .auth()
+      .verifyIdToken(req.header("user-token"))
+      .then((decodedToken) => {
+        const userId = decodedToken.uid;
+        req.body.user_id = userId;
+        next();
+      })
+      .catch((error) => {
+        return res.status(401).json({ message: "Unauthorized" });
+      });
   } else {
     return res.status(401).json({ message: "Unauthorized" });
   }
 };
+
+module.exports = userAuthorization;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userRoute = require("./user-route");
+const classRoute = require("./class-route");
 
 router.use('/user', userRoute);
+router.use('/class',classRoute);
 
 
 module.exports = router;
