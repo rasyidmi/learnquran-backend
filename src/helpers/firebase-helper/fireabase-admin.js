a6b19f76a6b52c23007caa3d9e02b6635ebbae00
Rasyid Miftahul Ihsan <rasyidmihsan@gmail.com>
2022-10-26 14:05:35 +0700

    REFACTOR: add error handler when firebase user already existed.

--- src/helpers/firebase-helper/fireabase-admin.js
+++ src/helpers/firebase-helper/fireabase-admin.js
@@ -2,10 +2,18 @@ const firebaseAdmin = require("firebase-admin");
 
 class FirebaseAdmin {
   static async createFirebaseUser(email, password) {
-    return await firebaseAdmin.auth().createUser({
-      email: email,
-      password: password,
-    });
+    await firebaseAdmin
+      .auth()
+      .createUser({
+        email: email,
+        password: password,
+      })
+      .then((firebaseUser) => {
+        return firebaseUser;
+      })
+      .catch(() => {
+        return null;
+      });
   }
 
   static async deleteFirebaseUser(uid) {
