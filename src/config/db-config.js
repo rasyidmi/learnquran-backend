b13d2466ed9b11dfc44435aa1012fe1d697303c9
Rasyid Miftahul Ihsan <54924480+rasyidmi@users.noreply.github.com>
2022-10-26 20:41:00 +0700

    ADD: add migration when deployment. (#26)

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,13 +1,17 @@
+if (process.env.ENV != "dev")
+  require("dotenv").config({ path: "./env/development/app.env" });
+
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PWD,
-    DB: process.env.DB_NAME,
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  logging: "true",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
