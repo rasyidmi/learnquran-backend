2650149d329464359af21ad93c1de125b6449854
Rasyid Miftahul Ihsan <54924480+rasyidmi@users.noreply.github.com>
2022-10-27 16:23:30 +0700

    ADD: new feature Create Task. (#42)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const userRoute = require("./user-route");
 const classRoute = require("./class-route");
 const studentRoute = require("./student-route");
-
+const taskRoute = require("./task-route");
 
 router.use('/user', userRoute);
 router.use('/class',classRoute);
 router.use('/student', studentRoute);
+router.use('/task', taskRoute);
 
 
 module.exports = router;
