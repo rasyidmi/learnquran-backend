6fe2758971d5ebe03b9c6d275b3ff957e4981043
Rasyid Miftahul Ihsan <rasyidmihsan@gmail.com>
2022-11-07 09:16:13 +0700

    refactor: delete previous audio file when uploading the new one.

--- src/controllers/student-controller.js
+++ src/controllers/student-controller.js
@@ -1,5 +1,6 @@
 const studentModel = require("../models/student-model");
 const submissionModel = require("../models/submission-model");
+const firebaseStorage = require("../helpers/firebase-storage");
 const Response = require("../dto/response");
 
 class StudentController {
@@ -50,10 +51,18 @@ class StudentController {
   }
 
   static async uploadAudio(req, res, next) {
-    const audioFileName = req.file;
+    const audioFile = req.file;
     const submissionId = req.params.id;
 
     try {
+      const submissionInstance = await submissionModel.get(submissionId);
+      if (submissionInstance.dataValues.audio_file != null) {
+        // Delete current audio file
+        await firebaseStorage.deleteFile(
+          submissionInstance.dataValues.audio_file
+        );
+      }
+      const audioFileName = firebaseStorage.uploadFile(audioFile);
       await submissionModel.uploadAudio(submissionId, audioFileName);
       const response = Response.postResponse(
         "The system successfully uploaded the audio to the submission."
